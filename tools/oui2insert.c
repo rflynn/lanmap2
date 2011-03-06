01105d089c039412f6fd12dbe0f65d60ba85b2d8
pizza <parseerror@gmail.com>
2011-03-06 15:58:28 -0500

    update to work with current ouis

--- tools/oui2insert.c
+++ tools/oui2insert.c
@@ -7,7 +7,7 @@
  * Parse the XX-XX-XX CORPNAME pattern for each OUI entry and
  * produce SQL for sqlite3
  *
- * Usage: cat oui.txt | ./import-oui | sqlite3 db
+ * Usage: cat oui.txt | ./oui2insert | sqlite3 db
  */
 
 #include <stdio.h>
@@ -30,16 +30,18 @@ static void escape(char *wr, size_t wrlen, const char *rd, size_t rdlen)
 int main(int argc, char *argv[])
 {
   char line[1024];
+  printf("BEGIN TRANSACTION;\n");
   while (NULL != fgets(line, sizeof line, stdin)) {
     char corp[64],
-         corpesc[128]; /* must be 2x to account for ecapes */
+         corpesc[sizeof corp * 2]; /* must be 2x to account for ecapes */
     unsigned h[3];
     int cnt = sscanf(line, "%02x-%02x-%02x (hex) %63[^\r\n]\n", h+0, h+1, h+2, corp);
     if (4 == cnt) {
       escape(corpesc, sizeof corpesc, corp, strlen(corp));
-      printf("INSERT INTO oui VALUES('%02x:%02x:%02x','%s');\n", h[0], h[1], h[2], corpesc);
+      printf("INSERT OR IGNORE INTO oui VALUES('%02x:%02x:%02x','%s');\n", h[0], h[1], h[2], corpesc);
     }
   }
+  printf("COMMIT;\n");
   return 0;
 }
 
